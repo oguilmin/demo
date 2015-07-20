0a339f1684d7466d0a012d790f615e4b6fc08bdd
oguilmin <olivier.guilmin@gmail.com>
2015-07-20 21:14:57 +0200

    ajout d'un cout

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 // test
 #include <stdio>
-
+using namespace std;
 int main()
 {
 	int a;
@@ -11,6 +11,7 @@ int main()
 	float d;
 	char car;
 	car = 'a';
+	cout<<"ce programme ne compile certainement pas et est bien inutile"<<endl;
 	return 0;
 }
 
